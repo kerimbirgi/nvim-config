2aeb6c8235312ce79c81182d1b402198e6d56647
kerimbirgi <k.birgi@gmail.com>
2026-03-10 18:41:33 +0100

    add toggle for autocomplete, add scrollofflimit, remove plugin override example file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.scrolloff = 10
+
+-- Show <tab> and trailing spaces
+vim.opt.list = true
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
+vim.opt.confirm = true
---
+++ lua/plugins/override.lua
@@ -0,0 +1,42 @@
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    init = function()
+      vim.g.cmp_disabled = false
+    end,
+    opts = function(_, opts)
+      opts.enabled = function()
+        -- local context = require("cmp.config.context")
+        if vim.g.cmp_disabled == true then
+          return false
+        end
+        -- some other conditions (like not in commments) can go here
+        return not disabled
+      end
+    end,
+    keys = {
+      {
+        "<leader>ua",
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled
+          local msg = ""
+          if vim.g.cmp_disabled == true then
+            msg = "Autocompletion (cmp) disabled"
+          else
+            msg = "Autocompletion (cmp) enabled"
+          end
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = "toggle autocompletion",
+      },
+    },
+  },
+}
